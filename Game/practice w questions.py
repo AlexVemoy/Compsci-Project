2706f649771900a51b0606978584fe58ad998b9c
AlexVemoy <102859015+AlexVemoy@users.noreply.github.com>
2022-04-02 12:30:00 +0100

    Add files via upload

---
+++ Game/practice w questions.py
@@ -0,0 +1,18 @@
+# Write your code here :-)
+with open('Questions.txt', 'r') as f:
+
+    for line in f:
+        print(line, end='')
+
+
+    #f_contents=f.readline()
+    #print(f_contents)
+
+    #f_contents = f.read()
+    #print(f_contents)
+
+
+
+
+
+#youtube.com/watch?v=Uh2ebFW8OYM
